d2cc7e3a3b66f733599f0610e8c0f76a163cf387
Ryan <rc6150@gmail.com>
2023-01-11 10:53:28 +0800

    edited constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,2 +1,2 @@
 const openaiBaseUrl = "https://api.openai.com/v1";
-const openaiApiKey = "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";
+const openaiApiKey = "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";
